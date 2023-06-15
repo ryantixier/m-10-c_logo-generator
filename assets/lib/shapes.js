667db2e5ab556880b78a7330a316519840757d2d
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-06-15 15:27:33 -0700

    (SHAPES.JS) added render commands.

--- assets/lib/shapes.js
+++ assets/lib/shapes.js
@@ -1,13 +1,20 @@
 // DESCRIPTION
 // Exports `Triangle`, `Circle`, and `Square` classes
 
+// PSEUDOCODE
+// PSEUDOCODE
+
+// think of common characteristics for Shapes
+// specify shape characteristics
+
+// PSEUDOCODE
+// PSEUDOCODE
+
 class Shapes {
   // Shapes class; specific shape (Cir., Tri., Squ.) classes will inheret placeholderX values from Shapes class object
   // ... will add new, specific values to their respective object values
-  constructor(placeholder1, placeholder2, placeholder3) {
-    this.placeholder1 = placeholder1;
-    this.placeholder2 = placeholder2;
-    this.placeholder3 = placeholder3;
+  constructor(color) {
+    this.color = color;
   }
 }
 
@@ -15,18 +22,19 @@ class Circle extends Shapes {
   // creates Circle class; specifies that values will be adopted from Shapes class object
   constructor(
     // templates out the Circle class object values to be constructed
-    placeholder1,
-    placeholder2,
-    placeholder3,
-    placeholderCir1,
-    placeholderCir2
+    color,
+    text,
+    textColor
   ) {
-    super(placeholder1, placeholder2, placeholder3);
+    super(color);
     // reflects origin template - Shapes class object
-    this.placeholderCir1 = placeholderCir1;
+    this.text = text;
     // adds placeholderCir1 values to (this) Circle class object
-    this.placeholderCir2 = placeholderCir2;
+    this.textColor = textColor;
     // adds placeholderCir2 values to (this) Circle class object
+  }
+  render() {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="${this.color}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text></svg>`;
   }
 }
 
@@ -34,18 +42,19 @@ class Triangle extends Shapes {
   // creates Triangle class; specifies that values will be adopted from Shapes class object
   constructor(
     // templates out the Triangle class object values to be constructed
-    placeholder1,
-    placeholder2,
-    placeholder3,
-    placeholderTri1,
-    placeholderTri2
+    color,
+    text,
+    textColor
   ) {
-    super(placeholder1, placeholder2, placeholder3);
+    super(color);
     // reflects origin template - Shapes class object
-    this.placeholderTri1 = placeholderTri1;
-    // adds placeholderTri1 values to (this) Triangle class
-    this.placeholderTri2 = placeholderTri2;
+    this.text = text;
+
+    this.textColor = textColor;
     // adds placeholderTri2 values to (this) Triangle class
+  }
+  render() {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 18 244, 182 56, 182" fill="${this.color}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text></svg> `;
   }
 }
 
@@ -53,18 +62,19 @@ class Square extends Shapes {
   // creates Square class; specifies that values will be adopted from Shapes class object
   constructor(
     // templates out the Square class object values to be constructed
-    placeholder1,
-    placeholder2,
-    placeholder3,
-    placeholderSqu1,
-    placeholderSqu2
+    color,
+    text,
+    textColor
   ) {
-    super(placeholder1, placeholder2, placeholder3);
+    super(color);
     // reflects origin template - Shapes class object
-    this.placeholderSqu1 = placeholderSqu1;
-    // adds placeholderSqu1 values to (this) Square class
-    this.placeholderSqu2 = placeholderSqu2;
+    this.text = text;
+
+    this.textColor = textColor;
     // adds placeholderSqu2 values to (this) Square class
+  }
+  render() {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="90" y="40" width="120" height="120" fill="${this.color}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text> </svg>`;
   }
 }
 
@@ -77,8 +87,3 @@ const newTriangle = new Triangle(
 );
 
 console.log(newTriangle);
-
-// triangle.setColor("blue");
-// expect(triangle.render()).toEqual(
-//   '<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
-// );
