d3acb05d5f444eabcc265cd85fb7eb9a27643aa1
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-06-17 12:01:20 -0700

    (INDEX.JS) inline comments added

--- index.js
+++ index.js
@@ -2,31 +2,38 @@ const { Circle, Square, Triangle } = require("./assets/lib/shapes");
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+// calls on inquirer
 inquirer
+  // brings up questions in terminal
   .prompt([
     {
+      // shape type
       type: "list",
       message: "What shape do you want?",
       choices: ["circle", "triangle", "square"],
       name: "shape",
     },
     {
+      // shape color
       type: "input",
       message: "What color do you want your shape to be?",
       name: "shapeColor",
     },
     {
+      // text overlay
       type: "input",
       message:
         "Enter the text that you want to overlay your shape (max 3 characters).",
       name: "text",
     },
     {
+      // text color
       type: "input",
       message: "What color do you want your text to be?",
       name: "textColor",
     },
   ])
+  // inputs crietria selections specified user into the corresponding, custom fields
   .then((res) => {
     let shape;
     if (res.shape === "circle") {
@@ -38,8 +45,10 @@ inquirer
     if (res.shape === "square") {
       shape = new Square(res.shapeColor, res.text, res.textColor);
     }
+    // writes logo.svg file based on user specifications
     fs.writeFileSync("./assets/examples/logo.svg", shape.render());
   })
+  // confirms logo.svg has been generated
   .then(() => {
     console.log("Generated logo.svg");
   });
